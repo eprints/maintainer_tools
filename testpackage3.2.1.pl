300e5a75293591537be392462569a976ea39a1a5
cjg <cjg@9491667e-5006-0410-a446-efbe8990b998>
2010-04-28 17:25:39 +0000

    3.2.1 tester
    git-svn-id: https://svn.eprints.org/eprints/tools/release@5353 9491667e-5006-0410-a446-efbe8990b998

---
+++ testpackage3.2.1.pl
@@ -0,0 +1,168 @@
+#!/usr/bin/perl -w
+
+my $package = $ARGV[0];
+
+my $base = "/tmp/ep_testing";
+
+# step one clean up any old install
+
+clean_up();
+
+run( "mkdir $base" );
+
+run( "mkdir $base/src/" );
+
+run( "mkdir $base/install/" );
+
+run( "cp $package $base/src/eprints.tgz" );
+
+test_chdir( "$base/src" );
+
+run( "tar xzvf eprints.tgz" );
+
+my $dh;
+opendir( $dh, "$base/src" );
+my $dir;
+while( my $file = readdir( $dh ) )
+{
+	next unless -d "$base/src/$file";
+	next if $file eq ".";
+	next if $file eq "..";
+	$dir = "$base/src/$file";
+}
+closedir( $dh );
+
+if( !defined $dir )
+{
+	die( "Could not find untar'd eprints distribution." );
+}
+
+test_chdir( $dir );
+
+my $group = getgrgid($));
+my $user = getpwuid($>);
+
+run( "./configure --prefix=$base/install --with-user=$user --with-group=$group" );
+
+run( "./install.pl" );
+
+# Installed!
+
+
+
+
+
+
+
+test_chdir( "$base/install" );
+
+my $id = "ep_auto_test";
+my $bin = "$base/install/bin";
+run( "(".<<END.") | $bin/epadmin create" );
+echo ep_auto_test; 
+echo '';
+hostname; # host name
+echo '';
+echo ''; 
+echo ''; # URL path
+echo ''; # secure host (do not use one)
+echo cjg\@ecs.soton.ac.uk; # admin email
+echo My Archive; # archive name
+echo '';
+
+echo ''; # conf db?
+echo '';
+echo '';
+echo '';
+echo '';
+echo '';
+echo Secret23; #
+echo ''; # write db settings?
+
+echo ''; # create db?
+echo ''; # root username?
+echo ''; # root pass?
+echo ''; # create tables
+
+echo ''; # init user?
+echo ''; # username
+echo 'admin'; # user type
+echo 'admin'; # password
+echo 'chris\@totl.net'; # user email
+
+echo ''; 
+echo ''; 
+echo ''; 
+
+END
+
+
+run( "$base/install/testdata/bin/import_test_data ep_auto_test --verbose" );
+run( "$bin/generate_views ep_auto_test --verbose" );
+run( "$bin/generate_abstracts ep_auto_test --verbose" );
+
+run( "$bin/indexer start" );
+run( "$bin/indexer stop" );
+
+# run indexer through on level 5 verbose
+run( "$bin/indexer --once --notdaemon --loglevel 5 start" );
+
+#clean_up();
+
+exit 0;
+
+
+
+
+
+
+sub clean_up
+{
+	run( "rm -rf $base", 1 );
+	run( "mysqladmin drop ep_auto_test -u root --force", 1 );
+}
+
+
+sub run
+{
+	my( $cmd, $dont_check ) = @_;
+
+	print "% $cmd\n";
+
+	open( CMD, "$cmd|" ) || die "Couldn't run: $cmd";
+	while( <CMD> ) 
+	{ 
+		print ": $_"; 
+	}
+	close CMD;
+
+	if ($? == -1) 
+	{
+		print "failed to execute: $!\n";
+		exit( 1 ) unless( $dont_check );
+	}
+	elsif ($? & 127) 
+	{
+		printf( "child died with signal %d, %s coredump\n",
+			($? & 127),  ($? & 128) ? 'with' : 'without' );
+		exit( 1 ) unless( $dont_check );
+	}
+	else 
+	{
+		my $v = $? >> 8;
+		if( $v ) 
+		{
+			printf( "child exited with value %d\n", $v );
+			exit( 1 ) unless( $dont_check );
+		}
+	}
+
+}
+
+sub test_chdir
+{
+	my( $dir ) = @_;
+
+	print "Changing dir to: $dir\n";
+	chdir( $dir );
+}
